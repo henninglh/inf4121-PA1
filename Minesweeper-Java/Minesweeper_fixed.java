3e64a99d4e83dcc677928061b29262893880961f
Espen Adrian Jones <ep.jones92@gmail.com>
2016-03-08 18:26:26 +0100

    All paramters within recomended metrics, but somewhat uneven. Hard to even out avg. complexity and avg. depth

--- Minesweeper-Java/Minesweeper_fixed.java
+++ Minesweeper-Java/Minesweeper_fixed.java
@@ -1,73 +1,102 @@
 import java.util.Scanner;
 
-public class Minesweeper {
+public class Minesweeper_fixed {
+
+	static int GAME_EXIT = 1;
+	static int GAME_CONTINUE = 2;
+	static int GAME_RESTART = 3;
 
 	private static MineField field;
 	private static Ranking rank;	
 	public static void main(String[] args) {
 		rank=new Ranking();
 		mainMessage();
-		while(gameCountinue());
+		while(gameCountinue() != GAME_EXIT);
 		System.out.println("\nThank you for playing :) Have a nice day!");
 	}	
-	private static boolean gameCountinue() {
+
+	private static int gameCountinue() {
 		field = new MineField();
 		int result = 0;
-		while (true) {
+		int game_status = GAME_CONTINUE;
 
+		Scanner in = null;
+		String input = null;
+		Boolean game;
+
+
+		while(game_status == GAME_CONTINUE) {
 			field.show();
 			System.out.print("\nPlease enter your move(row col): ");
-			Scanner in = new Scanner(System.in);
-			String input = in.nextLine();
+			in = new Scanner(System.in);
+			input = in.nextLine();
 
-			if (input.equals("top")) {
-				rank.show();
-				continue;
-			}
-			if (input.equals("restart")) {
-				rank.recordName(result);
-				return true;
-			}
-			if (input.equals("exit")) {
-				rank.recordName(result);
-				return false;
-			}
-			if (field.legalMoveString(input)) {
-				result++;
-				if (result == 35) {
-					System.out.println("Congratulations you WON the game!");
-					{
-						rank.recordName(result);
-						return true;
-					}
-				}
-				continue;
-			}
-//			if(input.equals("exit")){
-//				rank.recordName(result);
-//				return false;
-//			}
-//			if(field.legalMoveString(input)){
-//				result++;
-//				if(result==35){
-//					System.out.println("Congratulations you WON the game!");
-//					rank.recordName(result);
-//					return true;
-//				}
-//				continue;
-//			}
-			else if (field.getBoom()) {
-					return boom();
-				}
+			game_status = checkInput(input, result);
 		}
-    }
 
-	private static boolean boom() { 
-		System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
-		rank.recordName(result);
-		return true;
+		return game_status;
 	}
 
+	/* Check the input from user, and call correspoing methods */
+	private static int checkInput(String input, int result) {
+		if (input.equals("top")) rank.show();
+		else if (input.equals("restart")) {
+			rank.recordName(result);
+			return GAME_RESTART;
+			//return restart(result);
+		}
+		else if (input.equals("exit")) {
+			rank.recordName(result);
+			return GAME_EXIT;
+			//return exit(result);
+		}
+		else if (field.legalMoveString(input)) { 
+			return legalMove(result);
+		}
+		else if (field.getBoom()) {
+			return doBoom(result);
+		}
+
+		return GAME_CONTINUE;
+	}
+
+	/* Restart game 
+	private static int restart(int result) {
+		rank.recordName(result);
+		return GAME_RESTART;
+	} */
+
+	/* Exit gamme 
+	private static int exit(int result) { 
+		rank.recordName(result);
+		return GAME_EXIT;
+	}
+	 */
+
+	/* Explode a bomb, meaning game is lost. Return and give message to user */
+	private static int doBoom(int result) {
+		System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
+		rank.recordName(result);
+		return GAME_RESTART;
+	}
+
+	/* Do a legal move */
+	private static int legalMove (int result) {
+		result++;
+		if(result == 35) {
+			return winGame(result);
+		}
+		return GAME_CONTINUE;
+	}
+
+	/* Game is won, return and give message to user */
+	private static int winGame(int result) {
+		System.out.println("Congratulations you WON the game!");
+		rank.recordName(result);
+		return GAME_RESTART;
+	}
+
+	/* Print info strings, with instructions for the game */
 	private static void mainMessage(){
 		System.out.println("Welcome to Minesweeper!");
 		System.out.println("To play just input some coordinates and try not to step ont mine :)");
