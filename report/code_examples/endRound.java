a02654edfa692bed4f186d6b9fbd0fe89996d780
Espen Adrian Jones <ep.jones92@gmail.com>
2016-03-11 19:25:12 +0100

    forgot

--- report/code_examples/endRound.java
+++ report/code_examples/endRound.java
@@ -1,6 +1,6 @@
 public static int endRound(int result, int code) {
 	if(code == GAME_LOST) {
-		System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
+		System.out.println("\nBoooooooooooooooooooooooooo!...");
 	} else if (code == GAME_WON) {
 		System.out.println("Congratulations you WON the game!");
 	}
